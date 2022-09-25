1c764000e1ab40f66eded8eb2ac15e00db672ed2
Wildfoe_Univ <dykyi@knu.ua>
2022-09-25 17:33:00 -0300

    lab_3

---
+++ lab_3/src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+class Main {
+
+    public static void main(String[] args){
+        Task_1 f = new Task_1();
+        f.setVisible(true);
+
+        Task_2 s = new Task_2();
+        s.setVisible(true);
+
+
+
+
+    }
+}
