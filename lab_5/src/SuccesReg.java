4e93f610d5142583cb28537f431b4aba81e43d78
Wildfoe_Univ <dykyi@knu.ua>
2022-11-13 19:00:39 -0400

    lab_5

---
+++ lab_5/src/SuccesReg.java
@@ -0,0 +1,35 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class SuccesReg extends JFrame {
+
+
+
+
+
+        Color BackgroundColor = new Color(0xC4BD97);
+        Font font28 = new Font("Times new roman", Font.BOLD, 20);
+
+        private final JLabel NoConnection = new JLabel("Зареєстрація успішна");
+
+        public SuccesReg(){
+            setBackground(BackgroundColor);
+            setTitle("Login_form");
+            setSize(700,600);
+            setLocation(new Point(300,200));
+            setLayout(null);
+            setResizable(false);
+            getContentPane().setBackground( BackgroundColor );
+
+
+            initComponent();
+        }
+        private void initComponent(){
+            NoConnection.setFont(font28);
+            NoConnection.setBackground(Color.GREEN);
+            NoConnection.setBounds(80,100,600,200);
+            add(NoConnection);
+
+
+        }
+}
